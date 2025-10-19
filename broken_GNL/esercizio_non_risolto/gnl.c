d76b343e66288d7bc12c7b9d59bd679357b5d9d9
gio-pc <gio@gio.gio>
2025-10-19 12:58:52 -0800

    ps

---
+++ broken_GNL/esercizio_non_risolto/gnl.c
@@ -0,0 +1,46 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int	str_append_mem(char **s1, char *s2, size_t size2)
+{
+	size_t	size1 = strlen(*s1);
+	char	*tmp = malloc(size2 + size1 + 1);
+	if (!tmp)
+		return 0;
+	memcpy(tmp, *s1, size1);
+	memcpy(tmp + size1, s2, size2);
+	tmp[size1 + size2] = 0;
+	free(*s1);
+	*s1 = tmp;
+	return 1;
+}
+
+int	str_append_str(char **s1, char *s2)
+{
+	return str_append_mem(s1, s2, strlen(s2));
+}
+
+char	*get_next_line(int fd)
+{
+	static char b[BUFFER_size] = "";
+	char	*ret = NULL;
+	char	*tmp = strchr(b ,'\n');
+
+	while(!tmp)
+	{
+		if(!str_append_str(&ret, b))
+			return NULL;
+		int	read_ret = read(fd, b, BUFFER_SIZE);
+		if(read_ret == -1)
+			return NULL;
+		b[read_ret] = 0;
+	}
+	if(!str_append_mem(&ret, b, tmp - b + 1))
+	{
+		free(ret);
+		return NULL;
+	}
+	return ret;
+}
+
